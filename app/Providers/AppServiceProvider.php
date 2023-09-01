e830556e57f26e628f7b351c099ec5e3e4095ede
John Oye <olugbengajohnoye@gmail.com>
2023-09-01 11:16:30 +0100

    config for replit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') == 'production') {
-            URL::forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
