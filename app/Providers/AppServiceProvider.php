3df19a1dd8e9b0e83c819b7edbbfd09e0f747664
John Oye <olugbengajohnoye@gmail.com>
2023-09-01 11:06:50 +0100

    config for replit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
+            URL::forceSchema('https');
         }
     }
 }
